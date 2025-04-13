3cf2d9db6669e52990480d1a1e4e400849c4ca87
Håvard Syslak <haavard.syslak@gmail.com>
2025-04-13 15:42:43 +0200

    main

--- main.py
+++ main.py
@@ -1,167 +1,139 @@
-from orientation import RotationQuaterion
-import numpy as np
-import scipy
-from mcap.reader import make_reader
 from mcap_protobuf.reader import read_protobuf_messages
-from state import LieState, NominalState
-from sigma_points import SigmaPoints, JulierSigmaPoints, MwereSigmaPoints, SimplexSigmaPoints
+import blueye.protocol as bp
+import numpy as np
+import ukfm
+from ukfm.models import DvlMeasurement, DepthMeasurement
 from scipy.spatial.transform import Rotation as Rot
-from ukf import UKFM, QUKF
-from be_telemetry import McapLogger
-from models import ImuModelQuat, DvlMeasurement, DepthMeasurement
-import json
-import traceback
-from tqdm import tqdm
+import manifpy as manif
+from google.protobuf.timestamp_pb2 import Timestamp
+from foxglove_schemas_protobuf.LocationFix_pb2 import LocationFix
+
+from foxglove import FoxgloveLogger
+import time
+
+class McapProtobufReader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.msg_iter = read_protobuf_messages(filename)
+
+    def get_next_message(self):
+        # Use self.msg_iter to get the next message
+        try:
+            message = next(self.msg_iter)
+            return message
+        except StopIteration:
+            return None
+
+    def __iter__(self):
+        return self.msg_iter
 
 
-def make_ukf():
-    model = ImuModelQuat(
-        gyro_std=0.001,
-        gyro_bias_std=3.5e-4,
-        gyro_bias_p=1e-16,
+def get_gnss_sensor(msg: bp.PositionEstimateTel):
+    GNSS_SENSOR_ID = 4
+    for sensor in msg.position_estimate.navigation_sensors:
+        if sensor.sensor_id == GNSS_SENSOR_ID:
+            return sensor
+    return None
 
-        acc_std=0.001,
-        acc_bias_std=5e-4,
-        acc_bias_p=1e-16
+
+def initial_state(reader):
+    first_dvl_msg_received = False
+    first_gnss_msg_received = False
+    first_dpeth_msg_received = False
+
+    velocity = np.zeros(3)
+    pos = np.zeros(3)
+    heading = 0.0
+
+    for message in reader:
+        match message.topic:
+            case "blueye.protocol.DvlVelocityTel":
+                if not first_dvl_msg_received:
+                    velocity = message.proto_msg.dvl_velocity.velocity
+                    vel = np.array([velocity.x, velocity.y, velocity.z])
+                    first_dvl_msg_received = True
+
+            case "blueye.protocol.PositionEstimateTel":
+                heading = message.proto_msg.position_estimate.heading
+                if not first_gnss_msg_received:
+                    gnss_sensor = get_gnss_sensor(message.proto_msg)
+                    if gnss_sensor.is_valid:
+                        first_gnss_msg_received = True
+                        pos[0] = gnss_sensor.northing
+                        pos[1] = gnss_sensor.easting
+
+            case "blueye.protocol.DepthTel":
+                if not first_dpeth_msg_received:
+                    first_dpeth_msg_received = True
+                    pos[1] = message.proto_msg.depth.value
+
+        if first_dvl_msg_received and first_gnss_msg_received and first_dpeth_msg_received:
+            return pos, vel, heading
+
+
+def run_ukf(ukf, reader):
+    print("asdfsdf")
+    try: 
+        logger = FoxgloveLogger("testing.mcap")
+        print("asdfsdf")
+        t = reader.get_next_message().log_time
+        while True:
+            time.sleep(0.001)
+            message = reader.get_next_message()
+
+            dt = 1 / 80
+            match message.topic:
+                case "blueye.protocol.CalibratedImuTel":
+                    # messge.log_time
+                    gyro = message.proto_msg.imu.gyroscope
+                    accel = message.proto_msg.imu.accelerometer
+                    u = np.array([gyro.x, gyro.y, gyro.z, accel.x, accel.y, accel.z])
+                    # ukf.propagate(u, dt)
+
+                case "blueye.protocol.DvlVelocityTel":
+                    vel = message.proto_msg.dvl_velocity.velocity
+                    vel = np.array([vel.x, vel.y, vel.z])
+                    measurement = DvlMeasurement(vel)
+
+                case "blueye.protocol.PositionEstimateTel":
+                    msg = make_location_fix(message)
+                    print("publishing")
+                    topic = f"gnss.foxglove.{msg.__name__}"
+                    logger.publish(topic, msg, message.log_time_ns, "foxglove.LocationFix")
+
+                case "blueye.protocol.DepthTel":
+                    pass
+    finally:
+        logger.close()
+
+
+def make_location_fix(message: bp.PositionEstimateTel):
+    log_time_ns = message.log_time_ns
+    seconds = log_time_ns // 1_000_000_000
+    nanos = log_time_ns % 1_000_000_000
+    proto_timestamp = Timestamp(seconds=seconds, nanos=nanos)
+    global_position = message.proto_msg.position_estimate.global_position
+
+    return LocationFix(
+        timestamp=proto_timestamp,
+        latitude=global_position.latitude,
+        longitude=global_position.longitude,
+        altitude=0.0,
     )
-
-    dim_x = 9 
-    dim_q = model.Q.shape[0]
-    dim_z = 3
-    # sigma_points = MwereSigmaPoints(dim_x + dim_q, alpha=1.e-3, kappa=3. - (dim_x + dim_q))
-    sigma_points = SimplexSigmaPoints(dim_x + dim_q)
-    P0 = np.eye(dim_x)
-    # noise_points = SigmaPoints(dim_q, alpha=1e-4, kappa=3-dim_q)
-    # sigma_points = SigmaPoints(dim_x, alpha=1e-2, kappa=3-dim_x)
-    # noise_points = JulierSigmaPoints(dim_q, alpha=1e-2)
-    # sigma_points = JulierSigmaPoints(dim_x, alpha=1e-2)
-    P0 = np.eye(dim_x) * 1e-9
-
-    R = Rot.from_euler("XYZ", [0, 0, -2.14])
-    q0 = Rot.from_euler("XYZ", [0, 0, -2.14]).as_quat(scalar_first=True)
-    q0 = RotationQuaterion.from_vec(q0)
-    v0 = q0.rotate_vec(np.array([-0.04, 0.024, -0.023]))
-    p0 = np.array([1.8, -4.3, 0.22])
-
-    # x0 = LieState(R=R, pos=p0, vel=v0)
-    x0 = NominalState(ori=q0, vel=v0, pos=p0)
-
-    # ukf = UKFM(
-    #     dim_x=dim_x,
-    #     dim_q=dim_q,
-    #     points=sigma_points,
-    #     noise_points=noise_points,
-    #     model=model,
-    #     x0=x0,
-    #     P0=P0,
-    # )
-
-    ukf = QUKF(model=model, dim_x=dim_x, dim_q=dim_q, x0=x0, P0=P0, sigma_points=sigma_points, Q=model.Q)
-
-    return ukf
-
-
-def main():
-    np.set_printoptions(precision=2, linewidth=999)
-    logger = McapLogger("ufk_test.mcap")
-    logger.start()
-
-    ukf = make_ukf()
-    has_pred = False
-    R_dvl = np.eye(3) * 2e-9
-    # R_dvl[-1] = 4e-2
-    R_depth = 1e-3
-    dvl_meas = DvlMeasurement(R_dvl)
-    depth_meas = DepthMeasurement(R_depth)
-
-    with open("./log_car_left_back.mcap", "rb") as f:
-        reader = make_reader(f)
-        t = 0
-        t_tot = 0
-        t0 = 0
-        for schema, channel, message in tqdm(reader.iter_messages()):
-            if not has_pred and channel.topic != "imu":
-                t0 = message.log_time
-                t = message.log_time
-                continue
-            if not has_pred:
-                t0 = message.log_time
-                t = message.log_time
-            try:
-                match channel.topic:
-                    case "imu":
-                        if not has_pred:
-                            dt = 0.05
-                            t = message.log_time
-                            has_pred = True
-                        else:
-                            dt = float(message.log_time - t) * 1e-9
-                            t = message.log_time
-                        msg = json.loads(message.data.decode())
-                        acc = msg["accelerometer"]
-                        gyro = msg["gyroscope"]
-                        u = np.array(
-                            [gyro["x"], gyro["y"], gyro["z"], acc["x"], acc["y"], acc["z"]]
-                        )
-
-                        ukf.propagate(u, dt)
-                        logger.log_message(
-                            "state", ukf.x.to_json().encode(), timestamp=message.log_time
-                        )
-
-                    case "dvl":
-                        msg = json.loads(message.data.decode())
-                        vel = msg["velocity"]
-                        dvl_meas.z = np.array([vel["x"], vel["y"], vel["z"]])
-                        # ukf.update(dvl_meas, 0)
-
-                        logger.log_message("dvl", message.data, message.log_time)
-                        logger.log_message("state", ukf.x.to_json().encode(), message.log_time)
-
-                    case "pos":
-                        msg = log_pos(message.data)
-                        logger.log_message("pos", msg, timestamp=message.log_time)
-
-                    case "depth":
-                        logger.log_message("depth", message.data, timestamp=message.log_time)
-                        msg = json.loads(message.data.decode())
-                        depth_meas.z = msg["value"]
-                        ukf.update(depth_meas, 0)
-
-            except Exception:
-                print(f"died at: {(t - t0) * 1e-9}")
-                logger.stop()
-                print(traceback.format_exc())
-                break
-
-    if logger.running:
-        logger.stop()
-
-
-def wrap_angle(angle):
-    return (angle + np.pi) % (2 * np.pi) - np.pi
-
-def log_pos(msg):
-    pos = json.loads(msg.decode())
-    pos_estimate = {
-        "northing": pos["northing"],
-        "easting": pos["easting"],
-        "heading": wrap_angle(pos["heading"]),
-        "surge_rate": pos["surge_rate"],
-        "sway_rate": pos["sway_rate"],
-        "yaw_rate": pos["yaw_rate"],
-        "ocean_current": pos["ocean_current"],
-        "odometer": pos["odometer"],
-        "is_valid": pos["is_valid"],
-        "global_position": {
-            "latitude": pos["global_position"]["latitude"],
-            "longitude": pos["global_position"]["longitude"],
-        },
-        "speed_over_ground": pos["speed_over_ground"],
-        "course_over_ground": pos["course_over_ground"],
-    }
-    return json.dumps(pos_estimate).encode()
 
 
 if __name__ == "__main__":
-    main()
+    # Create an instance of the reader
+    reader = McapProtobufReader("mcap/log_square_05.04.mcap")
+    # Read messages one by one
+    pos, vel, heading = initial_state(reader)
+    q = Rot.from_euler("XYZ", [0, 0, heading]).as_quat(scalar_first=True)
+    extended_pose = manif.SE_2_3(np.concatenate([pos, q, vel]))
+    g = np.array([0.0, 0.0, 9.822])
+    initial_state = ukfm.LieState(extended_pose, g=g)
+    P0 = np.eye(initial_state.dof()) * 1e-3
+
+    ukf = ukfm.make_ukf(initial_state, P0)
+    run_ukf(ukf, reader)
+
+
